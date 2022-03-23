b53c6b1cb5b73a54abbcce8dd1a076d78d94658e
salma45667 <salmamalak555555@gmail.com>
2022-03-23 18:17:01 +0300

    cw7

--- Random/script.js
+++ Random/script.js
@@ -1,3 +1,24 @@
 // project أنشئ كائن
 
+
+
+
+const project={
+    type:"tech",
+    users:"100",
+    biography(){
+        return "This is my project"
+    }
+}
 // randomProperty أنشئ دالة
+function randomProperty(obj) {
+    const keys = Object.keys(obj);
+
+    const random = Math.floor(Math.random() * keys.length);
+
+    console.log(keys[random]);
+
+    console.log(keys[`${random}`]);
+
+}
+randomProperty(project);
