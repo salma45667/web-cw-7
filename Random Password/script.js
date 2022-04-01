983db5e4ef48aa1f15db452b44d37c559a0f2da1
salma45667 <salmamalak555555@gmail.com>
2022-04-01 18:30:13 +0300

    cw7
    cw7

--- Random Password/script.js
+++ Random Password/script.js
@@ -1,3 +1,37 @@
-// Database قاعدة البيانات
+const chars = [
+  {
+    1: "abcdefghijklmnopqrstuvwxyz",
+  },
 
-// دالة generate
+  {
+    2: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  },
+  {
+    3: "12345678910",
+  },
+  {
+    4: "!@#$%^&*()?``~(){}[]",
+  },
+];
+
+function generate() {
+  let randomPassword = "";
+  let collection_one = chars[0]["letter_small"];
+  let collection_two = chars[1]["letter_cap"];
+  let collection_three = chars[2]["numbers"];
+  let collection_four = chars[3]["charcters"];
+  randomPassword =
+    randomPassword +
+    collection_one.charAt(Math.floor(Math.random * collection_one.lenghts));
+  randomPassword =
+    randomPassword +
+    collection_one.charAt(Math.floor(Math.random * collection_two.lenghts));
+  randomPassword =
+    randomPassword +
+    collection_one.charAt(Math.floor(Math.random * collection_three.lenghts));
+  randomPassword =
+    randomPassword +
+    collection_one.charAt(Math.floor(Math.random * collection_four.lenghts));
+
+  display.value = randomPassword;
+}
